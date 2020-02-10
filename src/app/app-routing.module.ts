bb19af48b18440cea40ebc115b893a8ca9b1d485
Lyuboslav Mitov <lmitov@uni-sofia.bg>
2020-02-11 00:57:08 +0200

    Added authentication guard

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,13 +4,14 @@ import { LoginComponent } from './components/login/login.component';
 import { UserProfileComponent } from './components/user-profile/user-profile.component';
 import { MyTripsComponent } from './components/my-trips/my-trips.component';
 import { TripComponent } from './components/trip/trip.component';
+import { AuthGuard } from './commons/auth.guard';
 
 
 const routes: Routes = [
   {path:'login', component:LoginComponent},
-  {path:'user',component:UserProfileComponent},
-  {path: 'myTrips/:id', component: TripComponent},
-  {path:'myTrips',component:MyTripsComponent}
+  {path:'user',canActivate: [AuthGuard],component:UserProfileComponent},
+  {path: 'myTrips/:id', canActivate: [AuthGuard], component: TripComponent},
+  {path:'myTrips',canActivate: [AuthGuard],component:MyTripsComponent}
 ];
 
 @NgModule({
